59ad717a344c55dfd96eca6c9fbdeb8e4f171507
Seidy Kante <seidykante21@gmail.com>
2024-04-02 15:47:13 +0300

    updata templates views

--- templates/views.py
+++ templates/views.py
@@ -1,61 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import transaction
 from SoftwareTesting.models import Metrics
 from SoftwareTesting.CloneRepository import CloneRepositoryForm
 from SoftwareTesting.tools import get_java_files
 from SoftwareTesting.Analyzer import Analyzer
-from django.db import transaction
 from SoftwareTesting.config import DIRECTORY
 
-
 def index(request):
-
     if request.method == "POST":
-
-        metric = CloneRepositoryForm(request.POST)
-
-        # print(len(metrics))
-
-        if metric.is_valid():
-            (is_cloned, message) = metric.clone_repo(DIRECTORY)
-            # print("INPUT URL: ", request.POST["url"])
-
-            java_files = get_java_files(DIRECTORY)
-            # index = 0
-            with transaction.atomic():
-                for file in java_files:
-                    analyzed_data = Analyzer.analyze(file)
-                    # index += 1
-                    metrics = Metrics(**analyzed_data)
-                    metrics.save()
-
-                    # print(f"\n==================={index}. {file} ===================\n")
-                    # print(analyzed_data)
-
-            metrics = Metrics.objects.all()
-
+        # Process form submission
+        clone_form = CloneRepositoryForm(request.POST)
+        if clone_form.is_valid():
+            # Clone repository and analyze Java files
+            is_cloned, message = clone_form.clone_repo(DIRECTORY)
             if is_cloned:
+                # If cloning was successful, analyze Java files and save metrics
+                java_files = get_java_files(DIRECTORY)
+                with transaction.atomic():
+                    for file in java_files:
+                        analyzed_data = Analyzer.analyze(file)
+                        metrics = Metrics(**analyzed_data)
+                        metrics.save()
+                # Fetch all metrics from the database
+                metrics = Metrics.objects.all()
+                # Render the result template with metrics
                 return render(request, "Result.html", {"metrics": metrics})
-
             else:
-                return HttpResponse(
-                    f"""<h1>⚠️Error Cloning Repository</h1>
-                        <h3>{message}</h3>
-                        <h4> Is repo already cloned ? <a href = 'result/'>See Result ⏩<a>  </h4>
-                        <a href='/'>Go Back 🔙</a>"""
+                # If cloning failed, display error message
+                error_message = (
+                    f"<div style='text-align: center; color: red;'>"
+                    f"<h1>⚠️{message}</h1>"
+                    f"<h3  style='color: green;'>Is repo already cloned? <a href='/result/'>See Result ⏩</a></h3>"
+                    f"<a href='/'>Go Back 🔙</a>"
+                    f"</div>"
                 )
-
+                return HttpResponse(error_message)
         else:
-            return HttpResponse(
-                """<h1>⛔Invalid Form</h1>
-                    <br>
-                    <h3>⚠️Please enter a valid GitHub URL</h3>
-                <a href='/'>Go Back 🔙</a>"""
+            # If form submission is invalid, display error message
+            error_message = (
+                "<div style='text-align: center; color: red;'>"
+                "<h1>⛔Invalid Form</h1>"
+                "<br>"
+                "<h3>⚠️Please enter a valid GitHub URL</h3>"
+                "<a href='/'>Go Back 🔙</a>"
+                "</div>"
             )
-
+            return HttpResponse(error_message)
+    # Render the index template for GET requests
     return render(request, "index.html")
 
 
 def result(request):
+    # Fetch all metrics from the database and render the result template
     metrics = Metrics.objects.all()
     return render(request, "Result.html", {"metrics": metrics})
